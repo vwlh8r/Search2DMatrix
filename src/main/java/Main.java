3089e1e28710a811b715f95114eed2730022d886
Jake <jacobdanielrises@gmail.com>
2022-12-06 23:48:38 -0500

    last version before I cleanup and majorly revise

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,14 @@ public class Main {
 
     public static void main(String... args) {
         Solution solution = new Solution();
-    //    int[][] matrix = {{1}};  //1
-     //   int [][] matrix ={{1,1}};  //target 2
-     //   int[][] matrix = {{1},{3}};
-     //  int[][] matrix = {{1,3,5,7},{10,11,16,20},{23,30,34,50}};  //3
-        int[][] matrix = {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}};  //11
-        int target =11;
+    // int[][] matrix = {{1}};  //1  //2
+   //    int [][] matrix ={{1,1}};  //target 2
+   //     int[][] matrix = {{1,3}};//3
+   //    int[][] matrix = {{1,3,5,7},{10,11,16,20},{23,30,34,50}};  //3
+     //   int[][] matrix = {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}};  //11
+     //   int [][] matrix = {{1},{3}}; //3
+        int [][] matrix = {{1,3,5}};
+        int target = 5;
         boolean answer = solution.searchMatrix(matrix, target);
         System.out.print(answer);
     }
--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -2,25 +2,49 @@ public class Solution {
 
     public boolean searchMatrix(int[][] matrix, int target) {
         int[][] matrix2D;
-        matrix2D =  matrix;
+        matrix2D = matrix;
+        boolean truth = false;
+        boolean no_result = false;
 
-        int t; int column=0;
-        for (t = 0; t < matrix2D[0].length; t++) {   //column traversal
-            if (matrix2D[0][t] >= target) {
-       //         t = ((t - 1)==0 ) ? 1 : 0;
-                t=t-1;
-                break;
+        if ((matrix2D.length == 1) && (matrix2D[0].length == 1)){
+            if (matrix2D[0][0] != target) {
+                return false;
             }
         }
 
-        try{
-            for (int y = 0; y < matrix2D.length; y++) {     //row traversal
-                if (matrix2D[column][y] == target) {
-                    return true;
-                }}}catch(ArrayIndexOutOfBoundsException e){
+        int column=0; int t = 0;
+        try {
+            for (t = 0; t < matrix2D[0].length; t++) { //down traversal
+                if (matrix2D.length == 1){column = 1; t =1; break;}
+                if (matrix2D[t][0] >= target) {
+                    t = t - 1;
+                    t = ((t - 1)==0 ) ? 1 : 0;
+                    column = t;
+                    no_result=false;
+                    break;
+                }
+                no_result = true;
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
             e.printStackTrace();
-
-        }return false;
+            if(no_result){truth = false;}
+            t = ((t - 1) == -1) ? 1 : 0;
+            column = t;
+        }
+        if(no_result){ truth = false;}
+        column = t;
+        int y=0;
+        try {
+            for (y = 0; y < matrix2D.length+1;y++) {//across traversal
+                if (matrix2D[column][y] == target) {
+                    truth = true;
+                }
+            }
+        } catch (ArrayIndexOutOfBoundsException f) {
+            f.printStackTrace();
+            if(matrix2D[column][y-1] == target){truth = true;}
+        }
+        return truth;
     }
 
 }
